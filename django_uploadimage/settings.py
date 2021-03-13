3aca768d92ecc6da4e101b09a26a9faf9d104a16
MelpCode <67125923+MelpCode@users.noreply.github.com>
2021-03-13 16:34:32 -0500

    Update settings.py

--- django_uploadimage/settings.py
+++ django_uploadimage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tf5x$^w3!#h1h7z!^n!$9i&5rzpl@#3l@bxyxy_=4^5jwy6f$y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
